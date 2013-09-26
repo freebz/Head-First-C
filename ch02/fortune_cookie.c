e7a8018b14f168dabaa21583550c31ae0921fd27
FreeBz <freebz@hananet.net>
2013-09-27 06:22:53 +0900

    Chapter 2.5 String theory

---
+++ ch02/fortune_cookie.c
@@ -0,0 +1,11 @@
+void fortune_cookie(char msg[])
+{
+  printf("Message reads: %s\n", msg);
+  printf("msg occupies %i bytes\n", sizeof(msg));
+}
+
+int main() {
+  char quote[] = "Cookies make you fat";
+  fortune_cookie(quote);
+  return 0;
+}
