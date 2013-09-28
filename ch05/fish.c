720ef3eb1929b40ee6c713f417f98a9d39527c3e
FreeBz <freebz@hananet.net>
2013-09-28 22:49:24 +0900

    fish v4

--- ch05/fish.c
+++ ch05/fish.c
@@ -1,6 +1,16 @@
+struct exercise {
+  const char *description;
+  float duration;
+};
+
+struct meal {
+  const char *ingredients;
+  float weight;
+};
+
 struct preferences {
-  const char *food;
-  float exercise_hours;
+  struct meal food;
+  struct exercise exercise;
 };
 
 struct fish {
@@ -23,16 +33,19 @@ void label(struct fish f)
 {
   printf("Name:%s\nSpecies:%s\n%i years old, %i teeth\n",
 	 f.name, f.species, f.teeth, f.age);
+  printf("Feed with %2.2f lbs of %s and allow to %s for %2.2f hours\n",
+	 f.care.food.weight, f.care.food.ingredients,
+	 f.care.exercise.description, f.care.exercise.duration);
 }
 
 int main()
 {
-  struct fish snappy = {"Snappy", "Piranha", 69, 4, {"Meat", 7.5}};
+  struct fish snappy = {"Snappy", "Piranha", 69, 4, {{"Meat", 0.2}, {"swim", 7.5}}};
   catalog(snappy);
   label(snappy);
 
-  printf("Snappy likes to eat %s", snappy.care.food);
-  printf("Snappy likes to exercise for %f hours", snappy.care.exercise_hours);
+  //printf("Snappy likes to eat %s", snappy.care.food);
+  //printf("Snappy likes to exercise for %f hours", snappy.care.exercise_hours);
 
   return 0;
 }
