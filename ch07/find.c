b176e2729f1e81daf34139d1f507550f89157ccc
FreeBz <freebz@hananet.net>
2013-09-29 00:30:36 +0900

    find v2

--- ch07/find.c
+++ ch07/find.c
@@ -2,22 +2,41 @@ int NUM_ADS = 7;
 char *ADS[] = {
   "William: SBM GSOH likes sports, TV, dining",
   "Matt: SWM NS likes art, movies, theater",
-  "Lusi: SLM ND likes books, theater, art",
+  "Luis: SLM ND likes books, theater, art",
   "Mike: DWM DS likes trucks, sports and bieber",
   "Peter: SAM likes chess, working out and art",
-  "Josh: SJM likes sports, movies and thearter",
+  "Josh: SJM likes sports, movies and theater",
   "Jed: DBM likes theater, books and dining"
 };
 
-void find()
+int sports_no_bieber(char *s)
+{
+  return strstr(s, "sports") && !strstr(s, "bieber");
+}
+
+int sports_or_workout(char *s)
+{
+  return strstr(s, "sports") || strstr(s, "working out");
+}
+
+int ns_theater(char *s)
+{
+  return strstr(s, "NS") && strstr(s, "theater");
+}
+
+int arts_theater_or_dining(char *s)
+{
+  return strstr(s, "arts") || strstr(s, "theater") || strstr(s, "dining");
+}
+
+void find(int (*match)(char *))
 {
   int i;
   puts("Search results:");
   puts("---------------------------------------");
 
   for (i = 0; i < NUM_ADS; i++) {
-    if (strstr(ADS[i], "sports")
-	&& !strstr(ADS[i], "bieber")) {
+    if (match(ADS[i])) {
       printf("%s\n", ADS[i]);
     }
   }
@@ -26,6 +45,9 @@ void find()
 
 int main()
 {
-  find();
+  find(sports_no_bieber);
+  find(sports_or_workout);
+  find(ns_theater);
+  find(arts_theater_or_dining);
   return 0;
 }
