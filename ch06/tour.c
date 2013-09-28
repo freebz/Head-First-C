c9b8b4037876bec64e23a45081783d23eba6ab29
FreeBz <freebz@hananet.net>
2013-09-29 00:13:25 +0900

    Chapter 6 Building bridges

--- ch06/tour.c
+++ ch06/tour.c
@@ -27,7 +27,36 @@ island* create(char *name)
   return i;
 }
 
+void release(island *start)
+{
+  island *i = start;
+  island *next = NULL;
+  for (; i != NULL; i = next) {
+    next = i->next;
+    free(i->name);
+    free(i);
+  }
+}
+
 int main()
+{
+  island *start = NULL;
+  island *i = NULL;
+  island *next = NULL;
+  char name[80];
+  for(; fgets(name, 80, stdin) != NULL; i = next) {
+    next = create(name);
+    if (start == NULL)
+      start = next;
+    if (i != NULL)
+      i->next = next;
+  }
+  display(start);
+  release(start);
+  return 0;
+}
+
+int main3()
 {
   char name[80];
   fgets(name, 80, stdin);
